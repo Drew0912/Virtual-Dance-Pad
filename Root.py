7ee93a9768c04ade3943460be6eb3ac0022f0042
Andrew Lee <leea0912@gmail.com>
2019-11-16 20:48:37 +0000

    Initial commit

---
+++ Root.py
@@ -0,0 +1,12 @@
+from tkinter import *
+
+root = Tk()
+root.resizable(0,0) #Removes the Maximise button.
+
+w = Label(root, text="Hello World!")
+w.pack()
+
+button = Button(root, text="Button", width='40')
+button.pack()
+
+root.mainloop()
