0acfbe74e6b462d277fdb0a25d66bea43a5e9b04
Andrew Lee <leea0912@gmail.com>
2019-12-01 00:43:53 +0000

    Created Project folder and updated VCCB.py

---
+++ Project/Tkinter.py
@@ -0,0 +1,6 @@
+import tkinter as tk
+import webbrowser, os
+
+class MainWindow:
+    def __init__(self, root):
+        self.root = root
---
+++ Project/VCCB.py
@@ -0,0 +1,69 @@
+import cv2 #OpenCV
+import numpy as np #numpy needed for OpenCV
+
+class VidCapture:
+    def __init__(self, video_source=0):
+        self.name = "Webcam" #Name of Capture
+
+        self.point1 = () #Defining variables needed to draw rectangle.
+        self.point2 = ()
+        self.drawing = False
+        self.drawingfinish = False
+
+        self.vid=cv2.VideoCapture(video_source) #Open Video Source
+        cv2.namedWindow(self.name)
+
+    def showFrame(self):
+        ret, frame = self.vid.read()
+        if self.point1 and self.point2:
+            cv2.rectangle(frame, self.point1, self.point2, (255,255,255)) #Draw Rectangle
+
+            self.xlength = abs(self.point1[0] - self.point2[0])
+            self.ylength = abs(self.point1[1] - self.point2[1])
+            self.thirdx = int(self.xlength/3)
+            self.thirdy = int(self.ylength/3)
+            
+            self.a = self.point1[0]+self.thirdx
+            self.b = self.point2[0]-self.thirdx
+            self.c = self.point1[1]+self.thirdy
+            self.d = self.point2[1]-self.thirdy
+
+            cv2.line(frame, (self.a, self.point1[1]), (self.a, self.point2[1]), (255,255,255))
+            cv2.line(frame, (self.b, self.point1[1]), (self.b, self.point2[1]), (255,255,255))
+            cv2.line(frame, (self.point1[0], self.c), (self.point2[0], self.c), (255,255,255))
+            cv2.line(frame, (self.point1[0], self.d), (self.point2[0], self.d), (255,255,255))
+
+        cv2.imshow(self.name, frame)
+   
+    def Click(self, event, x, y, flags, param):
+        global point1, point2, drawing, drawingcounter
+        if self.drawingfinish is False:
+            if event == cv2.EVENT_LBUTTONDOWN:
+                if self.drawing is False:
+                    self.drawing = True
+                    self.point1 = (x,y)
+                else:
+                    self.drawing = False
+                    self.drawingfinish = True
+            elif event == cv2.EVENT_MOUSEMOVE:
+                if self.drawing is True:
+                    self.point2 = (x,y)
+        if event == cv2.EVENT_RBUTTONDOWN: #Testing purpose since no GUI.
+            self.Reset()        
+
+    def Reset(self):
+        global point1, point2, drawingcounter
+        self.point1 = ()
+        self.point2 = ()
+        self.drawingfinish = False   
+        
+def main():
+    cameraFeed = VidCapture() 
+    cv2.setMouseCallback(cameraFeed.name, cameraFeed.Click) 
+    while(True):
+        cameraFeed.showFrame()
+        if cv2.waitKey(20) == 27:
+                break
+    cv2.destroyAllWindows()
+if __name__ == "__main__":
+    main()
