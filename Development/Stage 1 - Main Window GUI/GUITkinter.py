520a70f26d2138731ff2e2c7abab05dd30a3f89b
Drew0912 <57844831+Drew0912@users.noreply.github.com>
2020-01-11 23:44:40 +0000

    Added StartStop and Help functions.

--- Development/Stage 1 - Main Window GUI/GUITkinter.py
+++ Development/Stage 1 - Main Window GUI/GUITkinter.py
@@ -1,11 +1,23 @@
 import tkinter as tk #Imports Tkinter under the name tk.
 from tkinter import ttk #ComboBox Widget is part of ttk module in Tkinter.
 
+import webbrowser, os
+
 class MainWindow(): #Main Window Class
     def __init__(self, root):
         self.root = root
         self.root.title("Main Window") #Title of the Window.
         self.root.resizable(0,0) #The window created cannot change size.
+
+        def StartStop(): #Changes text of Button when pressed.
+            if self.StartStopButton["text"] == "Start":
+                self.StartStopButton["text"] = "Stop"
+            else:
+                self.StartStopButton["text"] = "Start"
+
+        def Help(): #Opens HTML file.
+            url = 'file://' + os.path.realpath('index.html')
+            webbrowser.open(url)        
 
         self.WebcamSelect = ttk.Combobox(root, width=30, state='readonly',
                             values=[
@@ -20,10 +32,10 @@ class MainWindow(): #Main Window Class
         self.Message.grid(row=1, column=0, columnspan=2)
         self.DisplayButton = tk.Button(root, text="Open Webcam", width=30, height=5)
         self.DisplayButton.grid(row=2, column=0, columnspan=2)
-        self.StartStopButton = tk.Button(root, text="Start", width=30, height=5)
+        self.StartStopButton = tk.Button(root, text="Start", width=30, height=5, command=StartStop)
         self.StartStopButton.grid(row=4, column=0, columnspan=2,
                             sticky=tk.W+tk.E+tk.N+tk.S, pady=(0,20), padx=10)
-        self.HelpButton = tk.Button(root, text="Help", height=5)
+        self.HelpButton = tk.Button(root, text="Help", height=5, command=Help)
         self.HelpButton.grid(row=3, column=1, sticky=tk.W+tk.E+tk.N+tk.S, padx=(0,10))
         self.CalibrateButton = tk.Button(root, text="Calibrate/Setup", height=5)
         self.CalibrateButton.grid(row=3, column=0, sticky=tk.W+tk.E+tk.N+tk.S, padx=(10,0))
