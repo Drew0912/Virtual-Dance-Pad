572e25e3d8b1936eb64b2f854eb2f8b9efe3f281
Andrew Lee <leea0912@gmail.com>
2020-01-11 00:59:09 +0000

    Added Development stages to repository.

---
+++ Development/Stage 1 - Main Window GUI/GUITkinter.py
@@ -0,0 +1,7 @@
+import tkinter as tk #Imports Tkinter under the name tk.
+from tkinter import ttk #ComboBox Widget is part of ttk module in Tkinter.
+
+root = tk.TK()
+root.title("Virtual Dance Pad - Main Window")
+
+root.resizable(0,0)
