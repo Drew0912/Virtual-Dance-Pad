100a03428833d6f68734f56f5c7fee72a4f5c0da
Drew0912 <57844831+Drew0912@users.noreply.github.com>
2020-01-11 17:02:12 +0000

    Added comments.

--- Development/Stage 1 - Main Window GUI/GUITkinter.py
+++ Development/Stage 1 - Main Window GUI/GUITkinter.py
@@ -1,10 +1,10 @@
 import tkinter as tk #Imports Tkinter under the name tk.
 from tkinter import ttk #ComboBox Widget is part of ttk module in Tkinter.
 
-root = tk.Tk()
-root.title("Main Window")
+root = tk.Tk() #Creates Tkinter window under the name root.
+root.title("Main Window") #Title of the Window.
 
-root.resizable(0,0)
+root.resizable(0,0) #The window created cannot change size.
 
 WebcamSelect = ttk.Combobox(root, width=30, state='readonly',
                             values=[
@@ -15,7 +15,7 @@ WebcamSelect = ttk.Combobox(root, width=30, state='readonly',
                                 "3",])
 WebcamSelect.grid(row=0, column=0, columnspan=2, pady=(20,10))
 WebcamSelect.current(0)
-Message = tk.Label(root, text="") #Error message.
+Message = tk.Label(root, text="") #Spare Label to give message to user on input.
 Message.grid(row=1, column=0, columnspan=2)
 DisplayButton = tk.Button(root, text="Open Webcam", width=30, height=5)
 DisplayButton.grid(row=2, column=0, columnspan=2)
@@ -27,4 +27,4 @@ HelpButton.grid(row=3, column=1, sticky=tk.W+tk.E+tk.N+tk.S, padx=(0,10))
 CalibrateButton = tk.Button(root, text="Calibrate/Setup", height=5)
 CalibrateButton.grid(row=3, column=0, sticky=tk.W+tk.E+tk.N+tk.S, padx=(10,0))
 
-root.mainloop()
+root.mainloop() #Infinite loop that does not end until the window is closed.
