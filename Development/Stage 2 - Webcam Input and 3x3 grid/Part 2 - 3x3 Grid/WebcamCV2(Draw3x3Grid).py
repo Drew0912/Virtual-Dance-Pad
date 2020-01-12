4572c3ecbf3b07dc88d71a1fd05c4c584139b530
Drew0912 <57844831+Drew0912@users.noreply.github.com>
2020-01-12 20:24:10 +0000

    Added split into 3x3 grid

--- Development/Stage 2 - Webcam Input and 3x3 grid/Part 2 - 3x3 Grid/WebcamCV2(Draw3x3Grid).py
+++ Development/Stage 2 - Webcam Input and 3x3 grid/Part 2 - 3x3 Grid/WebcamCV2(Draw3x3Grid).py
@@ -8,6 +8,7 @@ class VidCapture:
         self.point1 = () #Tuple for first coordinate.
         self.point2 = () #Tuple for second coordinate.
         self.drawing = False #Boolean to know if user is drawing the rectangle.
+        self.drawingfinish = False
 
         self.vid = cv2.VideoCapture(video_source) #Gets the webcam and puts it under the name vid.
         cv2.namedWindow(self.Name) #Initialises an OpenCV Window
@@ -17,19 +18,44 @@ class VidCapture:
         if self.point1 and self.point2:
             cv2.rectangle(frame, self.point1, self.point2, (255,255,255)) #Draws Rectangle, colour white.
 
+            self.xlength = abs(self.point1[0] - self.point2[0]) #Horizontal Length
+            self.ylength = abs(self.point1[1] - self.point2[1]) #Vertical Length
+            self.thirdx = int(self.xlength/3) #Horizontal Length divided by 3
+            self.thirdy = int(self.ylength/3) #Vertical Length divided by 3
+
+            self.a = self.point1[0]+self.thirdx
+            self.b = self.point2[0]-self.thirdx
+            self.c = self.point1[1]+self.thirdy
+            self.d = self.point2[1]-self.thirdy
+
+            cv2.line(frame, (self.a, self.point1[1]), (self.a, self.point2[1]), (255,255,255)) #Line from point 1 to 6
+            cv2.line(frame, (self.b, self.point1[1]), (self.b, self.point2[1]), (255,255,255)) #Line from point 2 to 5
+            cv2.line(frame, (self.point1[0], self.c), (self.point2[0], self.c), (255,255,255)) #Line from point 8 to 3
+            cv2.line(frame, (self.point1[0], self.d), (self.point2[0], self.d), (255,255,255)) #Line from point 7 to 4
+
         cv2.imshow(self.Name, frame) #Creates window called WebcamFeed and displays frame from webcam.
 
     def Click(self, event, x, y, flags, param): #Function that happens on MouseCallback.
-        global point1, point2, drawing
-        if event == cv2.EVENT_LBUTTONDOWN:
-            if self.drawing is False:
-                self.point1 = (x,y)
-                self.drawing = True
-            else:
-                self.drawing = False
-        elif event == cv2.EVENT_MOUSEMOVE:
-            if self.drawing == True:
-                self.point2 = (x,y)            
+        global point1, point2, drawing, drawingfinish
+        if self.drawingfinish == False:
+            if event == cv2.EVENT_LBUTTONDOWN:
+                if self.drawing is False:
+                    self.point1 = (x,y)
+                    self.drawing = True
+                else:
+                    self.drawing = False
+                    self.drawingfinish = True
+            elif event == cv2.EVENT_MOUSEMOVE:
+                if self.drawing == True:
+                    self.point2 = (x,y)
+        if event == cv2.EVENT_RBUTTONDOWN: #Temp Function.
+            self.Reset()
+
+    def Reset(self): #Reset Rectangle and allow drawing again.
+        global point1, point2, drawingfinish
+        self.point1 = ()
+        self.point2 = ()
+        self.drawingfinish = False                    
 
 def main():
     cameraFeed = VidCapture() #Creates instance of VidCapture called cameraFeed.
