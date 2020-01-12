ccf6ad34c5d7321ebcb23ee7f6ce53bfcda65697
Drew0912 <57844831+Drew0912@users.noreply.github.com>
2020-01-12 01:03:42 +0000

    Updated comment

--- Development/Stage 2 - Webcam Input and 3x3 grid/Part 1 - Webcam Input/WebcamCV2.py
+++ Development/Stage 2 - Webcam Input and 3x3 grid/Part 1 - Webcam Input/WebcamCV2.py
@@ -6,7 +6,7 @@ vid = cv2.VideoCapture(Video_Source) #Gets the webcam and puts it under the name
 
 while (True): #Infinite loop.
     ret, frame = vid.read() #Gets data from webcam
-    cv2.imshow("WebcamFeed", frame) #Creates window and displays frame from webcam.
+    cv2.imshow("WebcamFeed", frame) #Creates window called WebcamFeed and displays frame from webcam.
     if cv2.waitKey(20) == 27: #Press esc to close window.
         break
 cv2.destroyAllWindows() #Closes all OpenCV Windows.
