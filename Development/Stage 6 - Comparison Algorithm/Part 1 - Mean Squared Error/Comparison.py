24d8b114802c8db83775486cdeda866b3c06af9a
Drew0912 <57844831+Drew0912@users.noreply.github.com>
2020-02-06 21:14:25 +0000

    Reverted Changes

--- Development/Stage 6 - Comparison Algorithm/Part 1 - Mean Squared Error/Comparison.py
+++ Development/Stage 6 - Comparison Algorithm/Part 1 - Mean Squared Error/Comparison.py
@@ -1,14 +1,9 @@
 import numpy as np #Additional maths functions
 import cv2 #OpenCV
 
-from skimage.metrics import structural_similarity
-
 def mse(x, y): #Mean Squared Error
     return np.linalg.norm(x - y)
-
-def ssim(x, y):
-    return structural_similarity(x, y)    
-
+   
 def main():
     im1 = cv2.imread('Control_picture.jpg')
     im2 = cv2.imread('Control_picture1.jpg')
