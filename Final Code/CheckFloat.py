928bd77a3818ea1f8e0da13085411a9e826d8058
Drew0912 <57844831+Drew0912@users.noreply.github.com>
2020-04-03 21:47:00 +0100

    Neatened

--- Final Code/CheckFloat.py
+++ Final Code/CheckFloat.py
@@ -1,7 +1,14 @@
+#-------------------------------------------
+#Andrew Lee
+#File name: CheckFloat.py
+#-------------------------------------------
+
+#Check if value is a float/real function
+
 def CheckFloat(s):
     try:
         float(s)
         return True
     except ValueError:
-        return False            
-        
+        return False      
+              
--- Final Code/Comparison.py
+++ Final Code/Comparison.py
@@ -1,19 +1,28 @@
+#-------------------------------------------
+#Andrew Lee
+#File name: Comparison.py
+#-------------------------------------------
+#
+#
+#Imports
 import numpy as np #Additional maths functions
 import cv2 #OpenCV
-
 from skimage.metrics import structural_similarity
 
-def mse(x, y): #Mean Squared Error
+#Mean Squared Error
+def mse(x, y):
     return np.linalg.norm(x - y)
 
+#Structural Similarity Measure    
 def ssim(x, y):
     return structural_similarity(x, y, multichannel=True)    
+
 
 def main():
     im1 = cv2.imread('Control_picture.jpg')
     im2 = cv2.imread('Control_picture1.jpg')
-
     print(mse(im1, im2))
 
 if __name__ == "__main__":
-    main()        
+    main() 
+           
